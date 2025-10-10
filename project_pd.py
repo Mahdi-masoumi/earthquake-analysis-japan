fd4b8ba399d36f0298d209fb9af8b6f57c36276c
Behesht <bht.hatami@gmail.com>
2025-10-10 21:49:39 +0330

    Update project_pd.py

--- project_pd.py
+++ project_pd.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#task1_read
+#task1 read
 df = pd.read_csv("earthquake1.csv")
 
-#task2_shape
-print(df.shape)
+#task2 shape
+print("Number of rows and columns:", df.shape)
 
 #task3 data type & convert
 df['time'] = pd.to_datetime(df['time'], errors='ignore')
@@ -55,19 +55,35 @@ print(pivot)
 
 
 
-# 9. region
+#task9 region
 
 Region = df.groupby('region')
 
 #part1
 earthquake_count = Region.size()
+print("Number of earthquakes per region:")
 print(earthquake_count)
 
 #part2
 mean_depth_mag = Region[['depth', 'magnitude']].mean()
+print("Average depth and magnitude per region:")
 print(mean_depth_mag)
 
 #part3
 max_depth_mag = Region[['depth', 'magnitude']].max()
+print("Maximum depth and per region:")
 print(max_depth_mag)
+
+#part4
+earthquake_count.plot( kind = "bar", figsize=(12, 6), title = "Earthquakes per Region", color = "green" )
+plt.xlabel("Region")
+plt.ylabel("Number of Earthquakes")
+plt.show()
+
+
+
+
+
+
+
 
