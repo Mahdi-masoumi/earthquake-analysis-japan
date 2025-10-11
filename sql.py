adeff046f693077c0f975dc94499eb939573934b
nimaaslrousta <nimaaslrousta@gmail.com>
2025-10-11 21:21:30 +0330

    creating a table and working with files

--- sql.py
+++ sql.py
@@ -1,4 +1,16 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ARRAY, Date, Float, Text
+import pandas as pd
+
+
+
+df = pd.read_csv("/Users/Vengeance/Documents/GitHub/earthquake-analysis-japan/JAPAN_DATASET.csv")
+df.head()
+
+
+
+
+
+
 
 engine = create_engine('sqlite:///mydb.db')
 connection = engine.connect()
