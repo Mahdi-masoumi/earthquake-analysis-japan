67aa0684780f86accc872856ab725bdaf002ce6c
Behesht <bht.hatami@gmail.com>
2025-10-19 00:27:54 +0330

    Update Final_Preprocessing.py

--- Final_Preprocessing.py
+++ Final_Preprocessing.py
@@ -3,109 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 import re
 
-# csv files list
-csv_files = [
-    "JAPAN_USGS.csv",
-    "JAPAN_EMSC.csv",
-    "JAPAN_DATASET.csv",
-    "JAPAN_GEOFON.csv"
-]
+def preprocessing():
 
-dataframes = []
+    # csv files list
+    csv_files = [
+        "JAPAN_USGS.csv",
+        "JAPAN_EMSC.csv",
+        "JAPAN_DATASET.csv",
+        "JAPAN_GEOFON.csv"
+    ]
 
-
-for f in csv_files:
-    # task1 read
-    df = pd.read_csv(f)
-    cleaned_df = df.copy()
-    
+    dataframes = []
 
 
-    # task2 shape
-    print("Number of rows and columns:", cleaned_df.shape)
-
-
-    # rename columns
-    if "coordinates" in cleaned_df.columns:
-        cleaned_df["coordinates"] = cleaned_df["coordinates"].fillna("")
-        coordinates = cleaned_df["coordinates"].str.split(",", expand = True)
-        cleaned_df["longitude"] = coordinates[0].str.replace("°E", "").str.strip().astype(float)
-        cleaned_df["latitude"] = coordinates[1].str.replace("°N", "").str.strip().astype(float)
-        cleaned_df.drop(columns=["coordinates"], inplace = True)
-
-   
-
-    if "date_and_time" in cleaned_df.columns:
-        cleaned_df.rename(columns = {"date_and_time": "time"}, inplace = True)
-
-    if "magnitude" in cleaned_df.columns:
-        cleaned_df.rename(columns = {"magnitude": "mag"}, inplace = True)
-
-    if "region" in cleaned_df.columns:
-        cleaned_df.rename(columns = {"region": "place"}, inplace = True)
-
-    # print(cleaned_df.head(3))
-    # print(cleaned_df.columns)
-
-    
-
-    #task3 convert datetime  
-
-    formats = [ "%m/%d/%Y  %I:%M:%S %p", "%Y-%m-%dT%H:%M:%S.%fZ", "%m/%d/%Y %H:%M" ] 
-    def convert_datetime(series):
-        df["time"] = df["time"].astype(str).str.strip()
-        for f in formats:
-            try:
-                return pd.to_datetime(series, format = f)
-            except:
-                continue
-        return pd.NaT
-    
-    if f == "JAPAN_DATASET.csv":
-        cleaned_df["time"] = cleaned_df["time"].apply(convert_datetime)
-    else:
-        cleaned_df["time"]= pd.to_datetime(cleaned_df["time"])
-    # cleaned_df["time"] = cleaned_df["time"].apply(
-    # lambda x: convert_datetime(x) if isinstance(x, str) else x
-    # )
-    
-
-    print(cleaned_df["time"])
-    
-    #task3 conver float
-    numeric_columns = []
-    for column in cleaned_df.columns:  
-        try:
-            cleaned_df[column].astype(float)
-            numeric_columns.append(column)
-        except:
-            pass
-        
-    float_columns = ["latitude", "longitude", "mag", "depth"] # must be floats
-
-    all_float_columns = list(set(numeric_columns + float_columns)) 
-
-    for column in all_float_columns:
-        cleaned_df[column] = pd.to_numeric(cleaned_df[column], errors = "coerce")
-
-    # print(cleaned_df.dtypes)
-    # print(cleaned_df.head())
-
-        
-    # clean data (logical filters)
-    
-    cleaned_df.loc[(cleaned_df["mag"] < 0) | (cleaned_df["mag"] > 10), "mag"] = pd.NA # 0 < Magnitude < 10
-    
-    cleaned_df.loc[(cleaned_df["depth"] < 0) | (cleaned_df["depth"] > 700), "depth"] = pd.NA  # 0 < Depth < 700
-    
-    cleaned_df.loc[cleaned_df["latitude"] < 0, "latitude"] = pd.NA # latitude +
-
-    cleaned_df.loc[cleaned_df["longitude"] < 0, "longitude"] = pd.NA # longitude +
-    # print(cleaned_df[["time", "latitude", "longitude"]].isna().sum())
-
-    # print(cleaned_df)
-
-    #task4 NaN values
+    # functions 
 
     def missing_values(cleaned_df):
         cleaned_df = cleaned_df.dropna(subset = ["time", "latitude", "longitude"]) # drop NaN time, latitude and longitude
@@ -117,45 +28,7 @@ for f in csv_files:
 
             cleaned_df = cleaned_df.fillna(0) # fill remaining (0)
         return cleaned_df
-    cleaned_df = missing_values(cleaned_df)
-    # print(cleaned_df)
-    # print(cleaned_df[["time", "latitude", "longitude"]].isna().sum())
-   
-    
-    # #task5 month column
-    
-    cleaned_df["time"] = pd.to_datetime(cleaned_df["time"], errors = "coerce") 
-    cleaned_df["month"] = cleaned_df["time"].dt.month
 
-#task6 category column
-    def categorize(mag):
-        if mag < 4:
-            return "Weak"      
-        elif mag < 6:
-            return "Moderate"   
-        else:
-            return "Strong"     
-
-    cleaned_df["category"] = cleaned_df["mag"].apply(categorize)
-
-    # print(cleaned_df["month"])
-    # print(cleaned_df["category"])
-    
-
-
-    #task7 group by and calculate
-    pivot = pd.pivot_table(
-        cleaned_df,
-        values = "mag",
-        index = "month",
-        columns = "category",
-        aggfunc = ["mean", "count"],
-        fill_value = 0
-    )
-
-    print(pivot)
-
-    #task8 extract region
     def extract_usgs(place):
         if not isinstance(place, str) or place.strip() == "":
             return "Not Found"
@@ -171,7 +44,6 @@ for f in csv_files:
                 return "Not Found"
         else:
             return "Not Found"
-
 
     def extract_emsc_geofon(place):
         if not isinstance(place, str) or place.strip() == "":
@@ -205,7 +77,7 @@ for f in csv_files:
             return "Sea Of Japan"
         if "ryukyu islands" in place.lower():
             return "Ryukyu Islands"
-       
+        
 
         removing_words = [
             "off", "near", "offshore", "central",
@@ -224,94 +96,217 @@ for f in csv_files:
         
         return cleaned_place.title()
 
-    cleaned_df["region"] = cleaned_df["place"].copy()
+    def categorize(mag):
+        if mag < 4:
+            return "Weak"      
+        elif mag < 6:
+            return "Moderate"   
+        else:
+            return "Strong"   
 
-    if f == "JAPAN_USGS.csv":
-        cleaned_df["region"] = cleaned_df["region"].apply(extract_usgs) 
-    elif f == "JAPAN_EMSC.csv":
-        cleaned_df["region"] = cleaned_df["region"].apply(extract_emsc_geofon)  
-    elif f == "JAPAN_DATASET.csv":
-        cleaned_df["region"] = cleaned_df["region"].apply(extract_dataset)  
-    elif f == "JAPAN_GEOFON.csv":
-        cleaned_df["region"] = cleaned_df["region"].apply(extract_emsc_geofon)
-
-    # Numpy task
+    for f in csv_files:
 
 
-    tokyo_long = 139.6500
-    tokyo_lat = 35.6764
-
-    x = cleaned_df["longitude"].values
-    y = cleaned_df["latitude"].values
-
-    cleaned_df["tokyo_distance"] = np.sqrt((x - tokyo_long)**2 + (y - tokyo_lat)**2)
-
-    mags = cleaned_df["mag"].values
-    mean_mag = np.mean(mags)
-    print(mean_mag)
-    std_mag = np.std(mags)
-    print(std_mag)
-    percentiles_mag = np.percentile(mags, [25, 50, 75])
-    print(percentiles_mag)
-
-    mean_distance = np.mean(cleaned_df["tokyo_distance"])
-    print(mean_distance)
-    std_distance = np.std(cleaned_df["tokyo_distance"])
-    print(std_distance)
-    percentiles_distance = np.percentile(cleaned_df["tokyo_distance"], [25, 50, 75])
-    print(percentiles_distance)
-
-    # region (ryukyu islands)
-    cleaned_df["region"] = cleaned_df["region"].replace("Ryukyu Isl.", "Ryukyu Islands")
-
-    # delete extra columns
-    # main_cols = ["time", "latitude", "longitude", "mag", "depth", "region", "place", "tokyo_distance"00]
-    # cleaned_df = cleaned_df[main_cols]
-    # print(cleaned_df.head(7))
-
-    #task9 
+        # task1 read
+        df = pd.read_csv(f)
+        cleaned_df = df.copy()
 
 
-    if f == "JAPAN_USGS.csv":
-        cleaned_df.to_csv("JAPAN_USGS_cleaned.csv", index = False)
-    elif f == "JAPAN_EMSC.csv":
-        cleaned_df.to_csv("JAPAN_EMSC_cleaned.csv", index = False)  
-    elif f == "JAPAN_DATASET.csv":
-        cleaned_df.to_csv("JAPAN_DATASET_cleaned.csv", index = False)   
-    elif f == "JAPAN_GEOFON.csv":
-        cleaned_df.to_csv("JAPAN_GEOFON_cleaned.csv", index = False)  
+        # task2 shape
+        print("Number of rows and columns:", cleaned_df.shape)
 
 
-    # append dfs
-    dataframes.append(cleaned_df)
+        # rename columns
+        if "coordinates" in cleaned_df.columns:
+            cleaned_df["coordinates"] = cleaned_df["coordinates"].fillna("")
+            coordinates = cleaned_df["coordinates"].str.split(",", expand = True)
+            cleaned_df["longitude"] = coordinates[0].str.replace("°E", "").str.strip().astype(float)
+            cleaned_df["latitude"] = coordinates[1].str.replace("°N", "").str.strip().astype(float)
+            cleaned_df.drop(columns=["coordinates"], inplace = True)
+
     
-all_dfs = pd.concat(dataframes, ignore_index = True)
-all_dfs = all_dfs.drop_duplicates()
 
-#task9
+        if "date_and_time" in cleaned_df.columns:
+            cleaned_df.rename(columns = {"date_and_time": "time"}, inplace = True)
 
-all_dfs = all_dfs[all_dfs["region"] != "Not Found"]
+        if "magnitude" in cleaned_df.columns:
+            cleaned_df.rename(columns = {"magnitude": "mag"}, inplace = True)
+
+        if "region" in cleaned_df.columns:
+            cleaned_df.rename(columns = {"region": "place"}, inplace = True)
 
 
-earthquake_count = all_dfs.groupby("region").size()
-print(earthquake_count)
+        #task3 convert datetime  
 
-earthquake_mean = all_dfs.groupby("region")[["mag", "depth"]].mean()
-print(earthquake_mean)
+        formats = [ "%m/%d/%Y  %I:%M:%S %p", "%Y-%m-%dT%H:%M:%S.%fZ", "%m/%d/%Y %H:%M" ] 
+        def convert_datetime(series):
+            df["time"] = df["time"].astype(str).str.strip()
+            for f in formats:
+                try:
+                    return pd.to_datetime(series, format = f)
+                except:
+                    continue
+            return pd.NaT
+        
+        if f == "JAPAN_DATASET.csv":
+            cleaned_df["time"] = cleaned_df["time"].apply(convert_datetime)
+        else:
+            cleaned_df["time"]= pd.to_datetime(cleaned_df["time"])
 
-earthquake_max = all_dfs.groupby("region")[["mag", "depth"]].max()
-print(earthquake_max)
+        
+        
+        #task3 convert float
+        numeric_columns = []
+        for column in cleaned_df.columns:  
+            try:
+                cleaned_df[column].astype(float)
+                numeric_columns.append(column)
+            except:
+                pass
+            
+        float_columns = ["latitude", "longitude", "mag", "depth"] # must be floats
 
-earthquake_count.plot.bar(
-    color = "gray",
-    title = "Earthquakes per Region",
-    xlabel = "Region",
-    ylabel = "Number of Earthquakes",
-    figsize = (6, 6)
-)
-plt.xticks(rotation = 90, ha = "right")
+        all_float_columns = list(set(numeric_columns + float_columns)) 
 
-plt.tight_layout()
-plt.show()
+        for column in all_float_columns:
+            cleaned_df[column] = pd.to_numeric(cleaned_df[column], errors = "coerce")
 
-# all_dfs.to_csv("earthquake_cleaned.csv", index = False)
+
+            
+        # clean data (logical filters)
+        
+        cleaned_df.loc[(cleaned_df["mag"] < 0) | (cleaned_df["mag"] > 10), "mag"] = pd.NA # 0 < Magnitude < 10
+        
+        cleaned_df.loc[(cleaned_df["depth"] < 0) | (cleaned_df["depth"] > 700), "depth"] = pd.NA  # 0 < Depth < 700
+        
+        cleaned_df.loc[cleaned_df["latitude"] < 0, "latitude"] = pd.NA # latitude +
+
+        cleaned_df.loc[cleaned_df["longitude"] < 0, "longitude"] = pd.NA # longitude +
+
+
+    #task4 NaN values
+
+
+        cleaned_df = missing_values(cleaned_df)
+        # print(cleaned_df)
+        # print(cleaned_df[["time", "latitude", "longitude"]].isna().sum())
+    
+        
+    #task5 month column
+        
+        cleaned_df["time"] = pd.to_datetime(cleaned_df["time"], errors = "coerce") 
+        cleaned_df["month"] = cleaned_df["time"].dt.month
+
+    #task6 category column
+    
+
+        cleaned_df["category"] = cleaned_df["mag"].apply(categorize)
+
+
+        
+
+
+    #task7 group by and calculate
+        pivot = pd.pivot_table(
+            cleaned_df,
+            values = "mag",
+            index = "month",
+            columns = "category",
+            aggfunc = ["mean", "count"],
+            fill_value = 0
+        )
+
+        print(pivot)
+
+    #task8 extract region
+
+
+        cleaned_df["region"] = cleaned_df["place"].copy()
+
+        if f == "JAPAN_USGS.csv":
+            cleaned_df["region"] = cleaned_df["region"].apply(extract_usgs) 
+        elif f == "JAPAN_EMSC.csv":
+            cleaned_df["region"] = cleaned_df["region"].apply(extract_emsc_geofon)  
+        elif f == "JAPAN_DATASET.csv":
+            cleaned_df["region"] = cleaned_df["region"].apply(extract_dataset)  
+        elif f == "JAPAN_GEOFON.csv":
+            cleaned_df["region"] = cleaned_df["region"].apply(extract_emsc_geofon)
+
+        # Numpy task
+
+
+        tokyo_long = 139.6500
+        tokyo_lat = 35.6764
+
+        x = cleaned_df["longitude"].values
+        y = cleaned_df["latitude"].values
+
+        cleaned_df["tokyo_distance"] = np.sqrt((x - tokyo_long)**2 + (y - tokyo_lat)**2)
+
+        mags = cleaned_df["mag"].values
+        mean_mag = np.mean(mags)
+        print(mean_mag)
+        std_mag = np.std(mags)
+        print(std_mag)
+        percentiles_mag = np.percentile(mags, [25, 50, 75])
+        print(percentiles_mag)
+
+        mean_distance = np.mean(cleaned_df["tokyo_distance"])
+        print(mean_distance)
+        std_distance = np.std(cleaned_df["tokyo_distance"])
+        print(std_distance)
+        percentiles_distance = np.percentile(cleaned_df["tokyo_distance"], [25, 50, 75])
+        print(percentiles_distance)
+
+        # region (ryukyu islands)
+        cleaned_df["region"] = cleaned_df["region"].replace("Ryukyu Isl.", "Ryukyu Islands")
+
+
+
+        #task8
+
+
+        if f == "JAPAN_USGS.csv":
+            cleaned_df.to_csv("JAPAN_USGS_cleaned.csv", index = False)
+        elif f == "JAPAN_EMSC.csv":
+            cleaned_df.to_csv("JAPAN_EMSC_cleaned.csv", index = False)  
+        elif f == "JAPAN_DATASET.csv":
+            cleaned_df.to_csv("JAPAN_DATASET_cleaned.csv", index = False)   
+        elif f == "JAPAN_GEOFON.csv":
+            cleaned_df.to_csv("JAPAN_GEOFON_cleaned.csv", index = False)  
+
+
+        # append dfs
+        dataframes.append(cleaned_df)
+        
+    all_dfs = pd.concat(dataframes, ignore_index = True)
+    all_dfs = all_dfs.drop_duplicates()
+
+    #task9
+
+    all_dfs = all_dfs[all_dfs["region"] != "Not Found"]
+
+
+    earthquake_count = all_dfs.groupby("region").size()
+    print(earthquake_count)
+
+    earthquake_mean = all_dfs.groupby("region")[["mag", "depth"]].mean()
+    print(earthquake_mean)
+
+    earthquake_max = all_dfs.groupby("region")[["mag", "depth"]].max()
+    print(earthquake_max)
+
+    earthquake_count.plot.bar(
+        color = "gray",
+        title = "Earthquakes per Region",
+        xlabel = "Region",
+        ylabel = "Number of Earthquakes",
+        figsize = (6, 6)
+    )
+    plt.xticks(rotation = 90, ha = "right")
+
+    plt.tight_layout()
+    plt.show()
+
+preprocessing()
+
