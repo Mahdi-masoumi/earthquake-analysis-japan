6cdebaa0e4d8713ec8b290b15899e75bb6e097af
kiakayemen <kiarashkarimian@outlook.com>
2025-10-18 22:14:41 +0330

    Remove outdated earthquake data scraping scripts and CSV files; implement new data collection methods for EMSC and GEOFON; consolidate USGS data retrieval; create a unified dataset for Japan earthquake events.

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def main_panel():
+    while True:
+        print("-------main menu-------")
+        main_menu_choice = input(
+            "enter 1-4:\n 1.get data\n 2.show query results\n 3.visualisations\n 4.unit testing\n")
+        if main_menu_choice not in [str(i) for i in range(1, 5)]:
+            raise ValueError(
+                "Invalid choice. Please enter a number between 1 and 4.")
+            break
