21e1534ef4bf460ca64d4d5b3fe2f59d59a74460
nimaaslrousta <nimaaslrousta@gmail.com>
2025-10-18 22:14:47 +0330

    final csv output

--- databasefinal.py
+++ databasefinal.py
@@ -115,3 +115,7 @@ result = connection.execute(
 ).fetchall()
 for row in result:
     print(row)
+
+output_path = "Earthquakes_export.csv"
+df_export = pd.read_sql("SELECT * FROM Earthquakes", con=engine)
+df_export.to_csv(output_path, index=False, encoding='utf-8-sig')
